5c4bb6b83632335963bc529197545e0f31420b8b
snowtrail115 <143370705+snowtrail115@users.noreply.github.com>
2023-08-28 17:27:27 -0400

    Update streamlit_app.py
    Added pandas library and reading from excel

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,3 +1,4 @@
+import pandas
 import streamlit
 streamlit.title('my patrents new healthy diner')
 streamlit.header('Breakfast Menu')
@@ -6,3 +7,4 @@ streamlit.text(' 🥗   Kale, Spinach & Rocket Smoothie')
 streamlit.text('🐔 Hard-Boiled Free-Range Egg')
 streamlit.text('🥑🍞 Avocado Toast')
 streamlit.header('🍌🥭 Build Your Own Fruit Smoothie 🥝🍇')
+my_fruit_list = pandas.read_csv("https://uni-lab-files.s3.us-west-2.amazonaws.com/dabw/fruit_macros.txt")
